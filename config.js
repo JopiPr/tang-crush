0c08f4f6cdebbacb62f5348cad24fcb2260efd5c
JopiPr <86725407+JopiPr@users.noreply.github.com>
2021-06-30 20:12:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Nào em đang ở đâu, anh qua chở công chúa của anh đi nè',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com.PhanQuocVietOfficial' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com.PhanQuocVietOfficial'
 }
